3ebe0d6d6ed015b7cdd401a4385c32b372ebfedc
Po-Shan Shih <tommyannypeter@gapp.nthu.edu.tw>
2018-04-11 23:08:08 +0800

    update Problem1.py

--- Problem1.py
+++ Problem1.py
@@ -1,21 +1,39 @@
 import random
 import time
+import math
 
 def OneDieS(trials, sides):
     c1 = time.clock()
+    mean_t = (1 + sides)/2
+    vari_t = (math.pow(sides, 2) - 1)/12
+    stde_t = math.sqrt(vari_t)
     print("====================")
     print("Number of sides = ", sides)
     print("Number of trials = ", trials)
+    print("Theoretical: ")
+    print("\t{:<20} = ".format("Mean value"), mean_t)
+    print("\t{:<20} = ".format("Variance"), vari_t)
+    print("\t{:<20} = ".format("Standard deviation"), stde_t)
 
     histogram = []
     for s in range(sides):
         histogram.append(0)
-    print(histogram)
 
     r = 0
+    total = 0
+    temp = 0 # calculate total r^2
     for t in range(trials):
-        r = int(random.random()*sides)
-        histogram[r] = histogram[r] + 1
+        r = int(random.random()*sides) + 1
+        histogram[r - 1] = histogram[r - 1] + 1
+        total += r
+        temp += math.pow(r, 2)
+    mean_s = total/trials
+    vari_s = temp/trials - math.pow(mean_s, 2)
+    stde_s = math.sqrt(vari_s)
+    print("Statistical:")
+    print("\t{:<20} = ".format("Mean value"), "{:<23}".format(mean_s), "(deviation = {:<23})".format(mean_t - mean_s))
+    print("\t{:<20} = ".format("Variance"), "{:<23}".format(vari_s), "(deviation = {:<23})".format(vari_t - vari_s))
+    print("\t{:<20} = ".format("Standard deviation"), "{:<23}".format(stde_s), "(deviation = {:<23})".format(stde_t - stde_s))
     print(histogram)
 
     print("s\t{:<9}".format("N_s") + "{:<23}".format("N_s-N/sides") + "{:<23}".format("N_s/N") + "{}".format("N_s/N-1/sides"))
